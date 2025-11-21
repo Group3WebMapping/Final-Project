4031274c80aa28b5e094882428fedd6a2fe6e4ba
mseaborn2020 <mseaborn2020@gmail.com>
2025-11-21 15:16:38 -0600

    added new page for demographic maps

---
+++ demographic_change.js
@@ -0,0 +1,31 @@
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer"
+], function (Map, MapView, FeatureLayer) {
+
+  const map = new Map({
+    basemap: "streets-navigation-vector"
+  });
+
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+
+    center: [-98.0, 30.0],
+    zoom: 10
+  });
+
+  const layerA = new FeatureLayer({
+    url: "https://services1.arcgis.com/M68M8H7oABBFs1Pf/arcgis/rest/services/Demographics2020WebMapping_Updated/FeatureServer/0",
+    opacity: 0.65
+  });
+
+  const layerB = new FeatureLayer({
+    url: "https://services1.arcgis.com/M68M8H7oABBFs1Pf/arcgis/rest/services/Demographics2010WebMapping_Updated/FeatureServer/0",
+    opacity: 0.65
+  });
+
+  map.addMany([layerA, layerB]);
+
+});
