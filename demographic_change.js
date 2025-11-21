f6cd5d398510c482d991e960ca1b7c8b16edcf59
mseaborn2020 <mseaborn2020@gmail.com>
2025-11-21 15:30:14 -0600

    adding popups for 2020

--- demographic_change.js
+++ demographic_change.js
@@ -18,6 +18,21 @@ require([
   const layerA = new FeatureLayer({
     url: "https://services1.arcgis.com/M68M8H7oABBFs1Pf/arcgis/rest/services/Demographics2020WebMapping_Updated/FeatureServer/0",
     opacity: 0.65
+    outFields: ["TRACTCE", "POP2020", "Median_Age2020", "PercentOccupiedHousing2020", "Median_Household_Income2020"],
+
+  popupTemplate: {
+    title: "2020 Demographics – Tract {TRACTCE}",
+    content: [{
+      type: "fields",
+      fieldInfos: [
+        { fieldName: "TRACTCE", label: "Census Tract" },
+        { fieldName: "POP2020", label: "Population (2020)" },
+        { fieldName: "Median_Age2020", label: "Median Age (2020)" }
+        { fieldName: "PercentOccupiedHousing2020", label: "% Occupied Housing (2020)" },
+        { fieldName: "Median_Household_Income2020", label: "Median Household Income (2020)" },
+      ]
+    }]
+  }
   });
 
   const layerB = new FeatureLayer({
