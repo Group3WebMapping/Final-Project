ab2bf32d66f6f630fd5745fa5793dfb191a090a8
mseaborn2020 <mseaborn2020@gmail.com>
2025-11-21 14:45:58 -0600

    add popup

--- main.js
+++ main.js
@@ -4,22 +4,33 @@ require([
   "esri/layers/FeatureLayer"
 ], function(Map, MapView, FeatureLayer) {
 
-  // Create the map
   const map = new Map({
     basemap: "streets-navigation-vector"
   });
 
-  // Create the view
   const view = new MapView({
     container: "viewDiv",
     map: map,
-    center: [-98.0, 30.0],   // Hays County area
+    center: [-98.0, 30.0],
     zoom: 10
   });
 
-  // Add the hosted feature layer
   const popLayer = new FeatureLayer({
-    url: "https://services1.arcgis.com/M68M8H7oABBFs1Pf/arcgis/rest/services/PopulationChange20102020Fixed/FeatureServer/0"
+    url: "https://services1.arcgis.com/M68M8H7oABBFs1Pf/arcgis/rest/services/PopulationChange20102020Fixed/FeatureServer/0",
+    outFields: ["TRACTCE", "POP2010","POP2020" "POPCHANGE"],
+
+  popupTemplate: {
+    title: "Census Tract {TRACTCE}",
+    content: [{
+      type: "fields",
+      fieldInfos: [
+        { fieldName: "TRACTCE", label: "Census Tract" },
+        { fieldName: "POP2010", label: "Population (2010)" },
+        { fieldName: "POP2020", label: "Population (2020)" },
+        { fieldName: "POPCHANGE", label: "Population Change (2010–2020)" }
+      ]
+    }]
+  }
   });
 
   map.add(popLayer);
