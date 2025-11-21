bff9eefecb6c53aace20158d3e01a0748864d2bb
mseaborn2020 <mseaborn2020@gmail.com>
2025-11-21 14:14:25 -0600

    added population change map to map.js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/FeatureLayer"
+], function(Map, MapView, FeatureLayer) {
 
+  // Create the map
+  const map = new Map({
+    basemap: "streets-navigation-vector"
+  });
+
+  // Create the view
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    center: [-98.0, 30.0],   // Hays County area
+    zoom: 9
+  });
+
+  // Add the hosted feature layer
+  const popLayer = new FeatureLayer({
+    url: "https://services1.arcgis.com/M68M8H7oABBFs1Pf/arcgis/rest/services/PopulationChange20102020Fixed/FeatureServer/0"
+  });
+
+  map.add(popLayer);
+});
